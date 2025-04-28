a356e7bf76d15fdcee7555d0b3b1db4483841c66
FarahAbdelkader <ffarah.ibrahim.mm@gmail.com>
2025-04-28 20:09:25 +0300

    Create LCD_interface.h

---
+++ Drivers/LCD/LCD_interface.h
@@ -0,0 +1,16 @@
+#ifndef LCD_interface
+#define LCD_interface
+
+void LCD_Init();
+void LCD_Enable();
+void LCD_Write(uint8_t DATA);
+void LCD_Send_CMD(uint8_t CMD);
+void LCD_Send_DATA(uint8_t DATA);
+void LCD_Send_Char(uint8_t Char);
+void LCD_Send_String(uint8_t *Str);
+void LCD_Clear();
+void LCD_Clear_Write_String(uint8_t *Str);
+void LCD_MoveCursor(uint8_t Row, uint8_t Col);
+
+
+#endif
