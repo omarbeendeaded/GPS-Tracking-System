86c6c0f2e7e44093955e6b3672d5dc620595c0c0
Mennaa-Ayman <mennaaradwan.4@gmail.com>
2025-04-30 14:51:00 +0300

    Add files via upload

--- Drivers/GPIO/GPIO_Interface.h
+++ Drivers/GPIO/GPIO_Interface.h
@@ -22,13 +22,13 @@ typedef enum{
 
 
 
-void GPIO_PortPin_Init(GPIO_Port_Select GPIOSEL, uint32_t pin); // enable one pin as a digital GPIO 
+void GPIO_Port_Init(GPIO_Port_Select GPIOSEL); // enable port pin as a digital GPIO 
 void GPIO_Digital_PIN_MODE(GPIO_Port_Select GPIOSEL, uint8_t pin, GPIO_Mode Mode, GPIO_Polarity Polarity); //Choose input or output and polarity
 void GPIO_Write_Pin(GPIO_Port_Select GPIOSEL, uint32_t pin, GPIO_Write data); // Write a value on a pin
 uint8_t MCAL_GPIO_Read_pin(GPIO_Port_Select GPIOSEL, uint32_t pin);  //Read the input data of a specific pin
 
 
-void GPIO_WritePort(GPIO_Port_Select port, uint8_t pin);   //Write on a port 
+void GPIO_WritePort(GPIO_Port_Select port, uint8_t data);   //Write on a port 
 uint8_t GPIO_ReadPort(GPIO_Port_Select GPIOSEL);    //Read from a port 
 
 // read port and write port function
