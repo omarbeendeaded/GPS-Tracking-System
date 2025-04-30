08a0569ae7fc7645085d1f2f43da782fff515127
Joseph484 <josephbassem123@gmail.com>
2025-04-30 11:39:05 +0300

    adding systick files

--- Drivers/SYSTICK/Systick.c
+++ Drivers/SYSTICK/Systick.c
@@ -1,7 +1,8 @@
-#include "Systick.h"
+#include "Systick_interface.h"
+#include "Systick_private.h"
 
 // for systick intialization:
-void SysTick_Init(){                  
+void Systick_Init(){                  
 NVIC_ST_CTRL_R = 0;                         //disable The SysTick
 NVIC_ST_RELOAD_R = 0xFFFFFF;                //maximum reload value 
 NVIC_ST_CURRENT_R = 0;                      //cleaning the current regiser " optional" 
@@ -9,7 +10,7 @@ NVIC_ST_CTRL_R = 0x05;                      //enable SysTick with the main clock
 } 
 
 //for the making a delay 
-void Systick_delay(uint_32t delay){
+void Systick_Wait (uint_32t delay){
 NVIC_ST_RELOAD_R = delay-1;                 // number of counts from 0 ----> (2^2n)-1 
 NVIC_ST_CURRENT_R = 0;                      //cleaning the current regiser " optional" 
 while((NVIC_ST_CTRL_R&0x00010000)==0){}	    // wait for the count flag to be set
@@ -20,11 +21,11 @@ while((NVIC_ST_CTRL_R&0x00010000)==0){}	    // wait for the count flag to be set
 void Systick_Wait_1ms (uint_32t delay){
 uint_32t i;
 for(i=0; i<delay; i++) 
-    Systick_delay(16000);                   // 10^-3 * 16*10^6 = 16000
+    Systick_Wait(16000);                   // 10^-3 * 16*10^6 = 16000
 }
 
 //for delay 1us using clock = 16 MHZ:
-void Systick_delay_1us (uint_32t delay){
+void Systick_Wait_1us (uint_32t delay){
 uint_32t i;
 for (i=0; i<delay; i++) 
     Systick_Wait(16);                       //106-6 * 16*10^6 = 16
--- Drivers/SYSTICK/Systick_interface.h
+++ Drivers/SYSTICK/Systick_interface.h
@@ -1,9 +1,9 @@
 #ifndef Systick_Interface
 #define Systick_Interface
 
-void SysTick_Init(void);
-void Systick_delay(uint32_t delay);
+void Systick_Init(void);
+void Systick_Wait(uint32_t delay);
 void Systick_Wait_1ms(uint32_t delay);
-void Systick_delay_1us(uint32_t delay);
+void Systick_Wait_1us(uint32_t delay);
 
 #endif
